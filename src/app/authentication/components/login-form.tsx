704fb0951cbfb191ae94de4822d57c4d943bd308
Junielson <juniosantos_@hotmail.com>
2025-07-11 01:38:06 -0400

    feat: add sign in with google

--- src/app/authentication/components/login-form.tsx
+++ src/app/authentication/components/login-form.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { createAuthClient } from "better-auth/react";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
@@ -25,7 +26,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
 
 const loginSchema = z.object({
   email: z.string().email({ message: "Email inválido" }),
@@ -35,6 +35,8 @@ const loginSchema = z.object({
 });
 
 const LoginForm = () => {
+  const authClient = createAuthClient();
+
   const router = useRouter();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
@@ -44,7 +46,7 @@ const LoginForm = () => {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof loginSchema>) {
+  async function handleSubmit(values: z.infer<typeof loginSchema>) {
     await authClient.signIn.email(
       {
         email: values.email,
@@ -61,10 +63,17 @@ const LoginForm = () => {
     );
   }
 
+  async function handleGoogleLogin() {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/dashboard",
+    });
+  }
+
   return (
     <Card>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
           <CardHeader>
             <CardTitle>Login</CardTitle>
             <CardDescription>Faça login para continuar</CardDescription>
@@ -103,17 +112,51 @@ const LoginForm = () => {
             />
           </CardContent>
           <CardFooter>
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={form.formState.isSubmitting}
-            >
-              {form.formState.isSubmitting ? (
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-              ) : (
-                "Entrar"
-              )}
-            </Button>
+            <div className="w-full space-y-2">
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  "Entrar"
+                )}
+              </Button>
+
+              <Button
+                variant="outline"
+                className="w-full"
+                type="button"
+                onClick={handleGoogleLogin}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="46"
+                  height="46"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#4285F4"
+                    d="M24 9.5c3.5 0 6.6 1.2 9 3.2l6.6-6.6C35.1 2.3 29.9 0 24 0 14.6 0 6.6 5.8 2.7 14.2l7.9 6.1C12.3 13.3 17.6 9.5 24 9.5z"
+                  />
+                  <path
+                    fill="#34A853"
+                    d="M46.1 24.5c0-1.6-.1-2.8-.4-4H24v7.6h12.4c-.5 3.1-2.1 5.8-4.5 7.6l7.1 5.5c4.2-3.8 6.6-9.4 6.6-16.7z"
+                  />
+                  <path
+                    fill="#FBBC05"
+                    d="M10.6 28.2c-.5-1.4-.8-2.9-.8-4.4s.3-3.1.8-4.4l-7.9-6.1C1.3 16.9 0 20.3 0 23.8s1.3 6.9 3.6 10z"
+                  />
+                  <path
+                    fill="#EA4335"
+                    d="M24 48c6.5 0 12-2.1 16-5.7l-7.1-5.5c-2.1 1.4-4.8 2.3-8.9 2.3-6.4 0-11.7-3.8-13.7-9.2l-7.9 6.1C6.6 42.2 14.6 48 24 48z"
+                  />
+                </svg>
+                Entrar com Google
+              </Button>
+            </div>
           </CardFooter>
         </form>
       </Form>
