bbc2c7044e84089090e4141a0361877fdf901723
Junielson <juniosantos_@hotmail.com>
2025-07-14 21:47:01 -0400

    feat: add sidebar

---
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import {
+  Calendar,
+  LayoutDashboard,
+  LogOut,
+  Stethoscope,
+  Users,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { authClient } from "@/lib/auth-client";
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Agendamentos",
+    url: "/appointments",
+    icon: Calendar,
+  },
+  {
+    title: "Médicos",
+    url: "/doctors",
+    icon: Stethoscope,
+  },
+  {
+    title: "Pacientes",
+    url: "/patients",
+    icon: Users,
+  },
+];
+
+const AppSidebar = () => {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/authentication");
+        },
+      },
+    });
+  };
+
+  return (
+    <Sidebar>
+      <SidebarHeader className="flex flex-row items-center border-b p-4">
+        <Image src="/logo.svg" alt="Odonto Hub" width={36} height={36} />
+        <h1 className="text-2xl leading-[140%] font-bold text-gray-600">
+          Odonto Hub
+        </h1>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu Principal</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger>Clíica</DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem onClick={handleSignOut}>
+                  <LogOut />
+                  Sair
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -6,7 +6,7 @@ import { db } from "@/db";
 import { usersToClinics } from "@/db/schema/usersToClinics";
 import { auth } from "@/lib/auth";
 
-import SignOutButton from "./components/sign-out-button";
+import SignOutButton from "./_components/sign-out-button";
 
 const Dashboard = async () => {
   const session = await auth.api.getSession({
