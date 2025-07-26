69e8f53b706c0f54d6e1c465bfdec8405aeb371f
Junielson <juniosantos_@hotmail.com>
2025-07-25 22:35:30 -0400

    feat: ensure patient can be created and updated

---
+++ src/app/(protected)/patients/page.tsx
@@ -0,0 +1,63 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { db } from "@/db";
+import { patients } from "@/db/schema/patients";
+import { auth } from "@/lib/auth";
+
+import AddPatientButton from "./_components/add-patient-button";
+import PatientCard from "./_components/patient-card";
+
+const Patients = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  if (!session?.user.clinic) {
+    redirect("/clinic-form");
+  }
+
+  const userPatients = await db.query.patients.findMany({
+    where: eq(patients.clinicId, session?.user?.clinic.id),
+  });
+
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Pacientes</PageTitle>
+          <PageDescription>
+            Gerencie os Pacientes da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+
+        <PageActions>
+          <AddPatientButton />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <div className="grid grid-cols-3 gap-6">
+          {userPatients.map((patient) => (
+            <PatientCard key={patient.id} patient={patient} />
+          ))}
+        </div>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default Patients;
