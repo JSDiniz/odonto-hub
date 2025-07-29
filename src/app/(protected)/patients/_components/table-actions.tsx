b0ed0c927244f5a8d2685f5532b06f756daced5f
Junielson <juniosantos_@hotmail.com>
2025-07-28 23:12:57 -0400

    feat: ensure patient can be updated

---
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -0,0 +1,55 @@
+import { EditIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { patients } from "@/db/schema/patients";
+
+import UpsertPatientForm from "./upsert-patient-form";
+
+interface PatientsActionsProps {
+  patient: typeof patients.$inferSelect;
+}
+
+const PatientsTableActions = ({ patient }: PatientsActionsProps) => {
+  const [upsertDialogIsOpen, setUpsertDialogIsOpen] = useState(false);
+
+  return (
+    <Dialog open={upsertDialogIsOpen} onOpenChange={setUpsertDialogIsOpen}>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost" size="icon">
+            <MoreVerticalIcon className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => setUpsertDialogIsOpen(true)}>
+            <EditIcon />
+            Editar
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <TrashIcon />
+            Excluir
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <UpsertPatientForm
+        patient={patient}
+        onSuccess={() => setUpsertDialogIsOpen(false)}
+      />
+    </Dialog>
+  );
+};
+
+export default PatientsTableActions;
